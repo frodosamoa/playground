b82ef161041f07078dcc0dc0577c15dfd5c332a6
frodosamoa <andrewrkowalczyk@gmail.com>
2013-03-29 21:04:41 -0700

    added Changemaker.java and Tuple.java

---
+++ changemaker/Changemaker.java
@@ -0,0 +1,133 @@
+public class Changemaker {
+
+    public static void main (String[]args) {
+        int amountOfCents = Integer.parseInt(args[0]);
+        int[] arrayOfDenominations = new int [args.length-1];
+        
+        /**
+           This checks to see if the user provided sufficient data, e.g., 
+           enough arguments.
+        */
+       
+        if (args.length <= 1) { throw new IllegalArgumentException("Insufficient Data"); }
+        
+        /**
+           This checks to see if the change to be sorted is a proper amount.
+        */
+        
+        if (amountOfCents < 1) { throw new IllegalArgumentException("Improper amount: " + amountOfCents); }
+        
+        /**
+           This checks the arguments after the amount of change to be made and
+           sees if the arguments are proper. If they are, then they are "added" to
+           the array of denominations.
+        */
+        for (int i = 1; i <args.length; i++) { 
+            if (Integer.parseInt(args[i]) <= 0) { 
+                throw new IllegalArgumentException("Improper Denomination: " + args[i]);
+            } else {
+                arrayOfDenominations[i-1] = Integer.parseInt(args[i]);
+            }
+        }
+       
+        /**
+           This checks to see if there are any suplicate denominations.
+        */
+        
+        for (int i = 0; i < arrayOfDenominations.length; i++) {
+            int denominationAtIndexI = arrayOfDenominations[i];
+            for (int j = 0; j < arrayOfDenominations.length; j++) {
+                if (j != i) {
+                    if (denominationAtIndexI == arrayOfDenominations[j]) {
+                        throw new IllegalArgumentException("Duplicate Denomination: " + denominationAtIndexI);
+                    }
+                }
+            }
+        }
+
+        Tuple denominations = new Tuple (arrayOfDenominations.length);
+        Tuple[][] table = new Tuple [arrayOfDenominations.length][amountOfCents+1];
+        changemaker(amountOfCents, arrayOfDenominations, denominations, table);
+        
+   }
+    
+    public static void changemaker (int amountOfCents, int[] arrayOfDenominations, Tuple denominations, Tuple[][] table) {
+        int finalCount = 0;
+        int tempDenom = 0;
+        String denomString = "";
+        
+        /**
+           This is the changemaking part of the program.
+           It uses the method discussed in class using a table.
+        */
+        
+        for (int i = 0; i < arrayOfDenominations.length; i++) {
+            tempDenom = arrayOfDenominations[i];
+            for (int j = 0; j < amountOfCents+1; j ++) {
+                if (j == 0) {
+                    table[i][j] = new Tuple (denominations.getDenomArray().length);
+                } else if (j >= tempDenom) {
+                    if (i == 0) {
+                        table[i][j] = new Tuple (denominations.getDenomArray().length);
+                        table[i][j].incrementByOneAtIndex(i);
+                        if (table[i][j-tempDenom].getDenomArray().length > 0) {
+                            table[i][j].add(table[i][j-tempDenom]);
+                        } else {
+                            table[i][j] = new Tuple (0);
+                        }
+                    } else {
+                        table[i][j] = new Tuple (denominations.getDenomArray().length);
+                        table[i][j].incrementByOneAtIndex(i);
+                        if (table[i][j-tempDenom].getDenomArray().length > 0) {
+                            table[i][j].add(table[i][j-tempDenom]);
+                        } else {
+                            table[i][j] = new Tuple (0);
+                        }
+                        if ((table[i-1][j].getDenomArray().length != 0) && table[i-1][j].isBetter(table[i][j])) {
+                            table[i][j] = table[i-1][j];
+                        }
+                        if ((table[i-1][j].getDenomArray().length != 0) && (table[i][j].getDenomArray().length == 0)) {
+                            table[i][j] = table[i-1][j];
+                        }
+                    }
+                } else {
+                    table[i][j] = new Tuple (0);
+                    if (i != 0) {
+                        if (table[i][j].getDenomArray().length == 0 && (table[i-1][j].getDenomArray().length > 0)) {
+                            table[i][j] = table[i-1][j];
+                        }
+                    }
+                }
+            }
+        }
+        
+        /**
+           This make a string representation of all of the denominations that are
+           being used.
+        */
+        
+        for (int i = 0; i <arrayOfDenominations.length; i++) {
+            if (i == arrayOfDenominations.length-1) {
+                denomString += arrayOfDenominations[i];
+            } else {
+                denomString += arrayOfDenominations[i] + ", ";
+            }
+        }
+        
+        /**
+           This tests to see if the bottom right tuple has a solution. If it
+           does, e.g., it has a length longer than 0, then is prints out the 
+           solution.
+        */
+        
+        if (table[arrayOfDenominations.length-1][amountOfCents].getDenomArray().length == 0) {
+            System.out.println("It is impossible to make " + amountOfCents + " cents with coins denominated " + denomString + ".");        
+        } else {
+            finalCount = table[arrayOfDenominations.length-1][amountOfCents].sum();
+            System.out.println("You can make " + amountOfCents + " cents with just " + finalCount + " coin(s) like this:");
+            for (int i = 0; i < denominations.getDenomArray().length; i++) {
+                System.out.println(table[arrayOfDenominations.length-1][amountOfCents].get(i) + " " + arrayOfDenominations[i] + "-cent coins");
+            }
+        }
+    }
+}
---
+++ changemaker/Tuple.java
@@ -0,0 +1,101 @@
+public class Tuple {
+
+    private int[] denominationArray;
+   
+    /**
+       Constructs a tuple with a given length denominationsLength, e.g., the
+       denominations of coins one is trying to make change with.
+    */
+    
+    public Tuple (int denominationsLength) {
+        denominationArray = new int[denominationsLength];
+        for (int i = 0; i < denominationsLength; i++) {
+            denominationArray[i] = 0;
+        }
+    }
+    
+    /**
+       Returns the current amounts of the denominations of a tuple.
+    */
+    
+    public int[] getDenomArray () {
+        return this.denominationArray;
+    }
+    
+    /**
+       Returns the current amount of a denomination of a tuple at an index i.
+    */
+    
+    public int get (int i) {
+        return this.denominationArray[i];
+    }
+    
+    /**
+       Set the current amount of a denominations to j of a tuple at an index i.
+    */
+    
+    public void set (int i, int j) {
+        this.denominationArray[i] = j; 
+    }
+    
+    /**
+       Increments the current amount of a denominations by one of a tuple at an index i.
+    */
+    
+    public void incrementByOneAtIndex (int i) {
+        this.denominationArray[i] = this.get(i) + 1;
+    }
+    
+    /**
+       Returns a stringy representation of a tuple.
+    */
+    
+    public String toString () {
+        String tupleString = "";
+        
+        for (int i = 0; i <this.getDenomArray().length; i++) {
+            if (i == this.getDenomArray().length-1) {
+                tupleString = tupleString + this.get(i);
+            } else {
+                tupleString = tupleString + this.get(i) + ",";
+            }
+        }
+        
+        tupleString = "[" + tupleString + "]";
+        return tupleString;
+    }
+    
+    /**
+       Adds two tuples together.
+    */
+    
+    public void add (Tuple t) {
+        for (int i = 0; i < this.denominationArray.length; i++) {
+            int j = this.get(i) + t.get(i);
+            this.set(i,j);
+        }
+    }
+    
+    /**
+       Returns the sum of all of the current denominations of a tuple.
+    */
+    
+    public int sum () {
+        int sumOfTuple = 0;
+        for (int i = 0; i <this.getDenomArray().length; i++) {
+            sumOfTuple += this.get(i);
+        }  
+        return sumOfTuple;
+    }
+    
+    /**
+       Returns true if a given tuple has less coins, e.g., a better solution.
+    */
+    
+    public boolean isBetter (Tuple t) {
+        boolean better = false;
+        if (this.sum() < t.sum()) { better = true; } 
+        return better;
+    }
+
+}
